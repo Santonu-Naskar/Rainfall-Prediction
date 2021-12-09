89b6981894cce1095b49eb61e09c68158e312d66
WiZerD0p <95866317+WiZerD0p@users.noreply.github.com>
2021-12-09 21:10:19 +0530

    Update main1.py

--- rainfall/main/main1.py
+++ rainfall/main/main1.py
@@ -8,9 +8,18 @@ import matplotlib.pyplot as plt
 
 # function call
 def cal():
+    print(var1.get())
     data = pd.read_csv("2a.csv")
+    df = pd.DataFrame(data,
+                      columns=['SUBDIVISION', 'YEAR', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP',
+                               'OCT', 'NOV', 'DEC'])
+
+    data= df.loc[df['SUBDIVISION'] == var1.get()]
+
     x = data['YEAR']
     x = x.values.reshape(-1, 1)
+    print(x)
+    # x=x.drop(['Unnamed: 0'],axis=1)
     if (var=='123'):
         showinfo("Invalid input", "please select a month")
     y = data[var.get()]
@@ -50,6 +59,11 @@ Radiobutton(root,text="Apr",variable=var, value="APR",font="any 12",foreground="
 Radiobutton(root,text="May",variable=var, value="MAY",font="any 12",foreground="blue").grid(row=7,column=2)
 Radiobutton(root,text="Jun",variable=var, value="JUN",font="any 12",foreground="blue").grid(row=8,column=2)
 
+obj=['ANDAMAN & NICOBAR ISLANDS', 'ARUNACHAL PRADESH', 'ASSAM & MEGHALAYA', 'NAGA MANI MIZO TRIPURA', 'GANGETIC WEST BENGAL', 'ORISSA', 'JHARKHAND', 'BIHAR', 'EAST UTTAR PRADESH', 'WEST UTTAR PRADESH', 'UTTARAKHAND', 'HARYANA DELHI & CHANDIGARH', 'PUNJAB', 'HIMACHAL PRADESH', 'JAMMU & KASHMIR', 'WEST RAJASTHAN' , 'EAST RAJASTHAN', 'WEST MADHYA PRADESH', 'EAST MADHYA PRADESH', 'GUJARAT REGION', 'SAURASHTRA & KUTCH', 'KONKAN & GOA', 'MADHYA MAHARASHTRA', 'MATATHWADA', 'VIDARBHA', 'CHHATTISGARH', 'COASTAL ANDHRA PRADESH', 'TELANGANA', 'RAYALSEEMA', 'TAMIL NADU', 'COASTAL KARNATAKA', 'NORTH INTERIOR KARNATAKA', 'SOUTH INTERIOR KARNATAKA', 'KERALA', 'LAKSHADWEEP',]
+var1=StringVar()
+var.set('ANDAMAN & NICOBAR ISLANDS')
 
-Button(text="Calculate Now", command=cal, activebackground = "yellow",border=5).grid(row=9,column=2,pady=20,ipadx=25)
+OptionMenu(root,var1,*obj).grid(row=9,column=2)
+Label(root, text="      Select -> :)",font="any 13 bold",foreground="#853535").grid(row=9,column=1)
+Button(text="Calculate Now", command=cal, activebackground = "yellow",border=5).grid(row=11,column=2,pady=20,ipadx=25)
 root.mainloop()
